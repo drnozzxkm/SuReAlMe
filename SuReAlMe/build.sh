c531e1e03de192cc80b0f004f124db4d40654168
drnozzxkm <drno.zzxkm1@gmail.com>
2025-10-21 03:39:39 -0500

    fix: corrected main app path again

--- SuReAlMe/build.sh
+++ SuReAlMe/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# Este script instala dependencias y recopila archivos estáticos
+# Esto le dice a Linux que use bash para ejecutar
 
+# Instalar dependencias
 pip install -r requirements.txt
+
+# Recoger archivos estáticos
 python manage.py collectstatic --noinput
+
+# Aplicar migraciones
+python manage.py migrate
